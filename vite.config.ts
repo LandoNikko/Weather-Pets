ab1011e60d7661a4e908fe00ba98dd3be4c13ac9
LandoNikko <landonikko@gmail.com>
2025-12-02 22:16:20 +0200

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/WEAPI/',
   server: {
     watch: {
       usePolling: true,
